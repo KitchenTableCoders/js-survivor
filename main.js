840d3900e967b7da6c37c71c794f66476c30a34b
David Nolen <david.nolen@gmail.com>
2013-11-23 11:59:44 -0500

    latest

--- main.js
+++ main.js
@@ -3,4 +3,11 @@
 
   x = 5;
 
+  var url =  "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+
+  function query(q) {
+    $.getJSON(url + q, null, function(xs) {
+    });
+  }
+
 })(this);
