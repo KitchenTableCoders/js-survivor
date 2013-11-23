ef22f54a952c5c020f99c843a77d9dac4817a6f2
David Nolen <david.nolen@gmail.com>
2013-11-23 16:01:05 -0500

    done!

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
-;(function () {
+;(function (global) {
   "use strict";
-
-  x = 5;
 
   var url =  "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
 
+  var resultsTemplate = _.template($("#search-results").text()),
+      templateCache   = {};
+
+  _.mixin({
+    partial: function(templateName, data) {
+      var partialTemplate = templateCache[templateName];
+      if(!partialTemplate) {
+        var templateEl = $("#"+templateName);
+        if(templateEl.length == 0) {
+          console.error("No template", templateName, "exists");
+        }
+        partialTemplate = _.template(templateEl.text());
+        templateCache[templateName] = partialTemplate
+      }
+      return partialTemplate(data);
+    }
+  });
+
+  function message(str) {
+    $("#message").html(str);
+  }
+
   function query(q) {
-    $.getJSON(url + q, null, function(xs) {
+    $.ajax({
+      url: url + q,
+      dataType: "jsonp",
+      success: function(jsonp) {
+        $("#results").html(
+          resultsTemplate({results: jsonp[1]})
+        );
+      }
     });
   }
+
+  var clearMessage = _.debounce(function() {
+    message("");
+  }, 3000);
+
+  function init() {
+    $("#search").on("click", function(e) {
+      query($("#query").prop("value"));
+    });
+
+    $("#results").delegate("input[type=checkbox]", "click", function(e) {
+      var id = $(e.target).parents("li").data("id");
+      message(id + " clicked!");
+      clearMessage();
+    });
+  }
+
+  init();
 
 })(this);
--- scratch.js
+++ scratch.js
@@ -1,6 +1,129 @@
-(function() {
+;(function(global) {
   "use strict"
 
-  console.log("Hello!");
-  
-})();
+  var NolenMath = {};
+  global.NolenMath = NolenMath;
+
+  function inc(n) {
+    return n+1;
+  }
+
+  function fooify(n) {
+    return "foo" + n;
+  }
+
+  function map(f, xs) {
+    var ret = [];
+
+    for(var i = 0; i < xs.length; i++) {
+      ret.push(f(xs[i]));
+    }
+
+    return ret;
+  }
+
+  function even(n) {
+    return (n % 2) == 0;
+  }
+
+  function not(f) {
+    return function(x) {
+      return !f(x);
+    };
+  }
+
+  function makePoint(x, y) {
+    return {
+      x: x,
+      y: y,
+      toString: function() {
+        return ["<", x, ", ", y, ">"].join("");
+      }
+    };
+  }
+
+  var Point = function(x, y) {
+    this.x = x;
+    this.y = y;
+  };
+
+  Point.prototype = {
+    numComponents: 2,
+    add: function(other) {
+      this.x += other.x;
+      this.y += other.y;
+    },
+    sub: function(other) {
+      this.x -= other.x;
+      this.y -= other.y;
+    }
+  };
+
+  var get = function(propertyName) {
+    return function(obj) {
+      return obj[propertyName];
+    };
+  };
+
+  // export
+  NolenMath.Point = Point; 
+
+  var data = [
+    new Point(5, 7.5), 
+    new Point(1.5, 2),
+    new Point(1.5, 9)
+  ];
+
+  var sum = function(acc, n) {
+    return acc + n;
+  };
+
+  var pointAdd = function(p1, p2) {
+    return new Point(p1.x+p2.x,p1.y+p2.y);
+  };
+
+  var badData = [
+     ["foos", 1, 2, 3],
+     ["bars", 5, 6, 7],
+     ["bazs", 8, 9, 0]
+  ];
+
+  var fixData = function(obj, dataElement) {
+    obj[dataElement[0]] = dataElement.slice(1);
+    return obj;
+  };
+
+  console.log(_([1,2,3,4,5]).reduce(sum, 0));
+  var goodData = _(badData).reduce(fixData, {});
+
+  console.log(goodData.foos);
+  console.log(goodData.bars);
+
+  function BaseComponent(name) {
+    this.name = name;
+  }
+  BaseComponent.prototype = {
+    foo: function() {
+      console.log("this is foo method", this.name);
+    },
+    baz: function() {
+      return 1;
+    }
+  }
+
+  function SubComponent(name, otherField) {
+    BaseComponent.call(this, name);
+    this.otherField = otherField;
+  }
+  SubComponent.prototype = new BaseComponent();
+  SubComponent.prototype.bar = function() {
+    console.log("this is bar method", this.otherField);
+  };
+
+  var myObject = new SubComponent("David", "cool!");
+  console.log(myObject);
+  myObject.foo();
+  myObject.bar();
+  console.log(myObject.baz());
+
+})(this);
